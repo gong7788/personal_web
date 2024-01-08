1e273bb609e1963001bd838a33f7ae5dc4b38ee8
gong7788 <g827029400@gmail.com>
2024-01-08 22:33:35 +0000

    login auth

--- app.py
+++ app.py
@@ -1,19 +1,31 @@
 from flask import Flask, render_template, request, url_for, redirect, flash
+from werkzeug.security import generate_password_hash, check_password_hash
 from flask_sqlalchemy import SQLAlchemy
 import click
 import os
 from markupsafe import escape
+from flask_login import LoginManager, UserMixin, login_user, login_required, logout_user, current_user
 
 app = Flask(__name__)
+login_manager = LoginManager(app)
+login_manager.login_view = 'login'
 
 app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:////' + os.path.join(app.root_path, 'data.db')
 app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False  # 关闭对模型修改的监控
 app.config['SECRET_KEY'] = 'dev'  # 等同于 app.secret_key = 'dev'
 db = SQLAlchemy(app)
 
-class User(db.Model):
+class User(db.Model, UserMixin):
     id = db.Column(db.Integer, primary_key=True)  # 主键
     name = db.Column(db.String(20))  # 名字
+    username = db.Column(db.String(20))
+    password_hash = db.Column(db.String(128))
+
+    def set_password_hash(self, password: str) -> None:
+        self.password_hash = generate_password_hash(password)
+
+    def check_password_hash(self, password: str) -> bool:
+        return check_password_hash(self.password_hash, password) 
 
 class Movie(db.Model):
     id = db.Column(db.Integer, primary_key=True)  # 主键
@@ -27,7 +39,11 @@ def inject_user():
 
 @app.route('/', methods=['GET', 'POST'])
 def index():
+    print(current_user.is_authenticated)
     if request.method == 'POST':
+        if not current_user.is_authenticated:
+            return redirect(url_for('index'))
+        
         name = request.form.get('name')
         year = request.form.get('year')
         # 验证数据
@@ -41,10 +57,12 @@ def index():
         flash('Item created.')  # 显示成功创建的提示
         return redirect(url_for('index'))  # 重定向回主页
     # user = User.query.first()
+    # because of context_processor, we don't need to pass user to template
     movies = Movie.query.all()
     return render_template('index.html', movies=movies)
 
 @app.route('/movie/edit/<int:movie_id>', methods=['GET', 'POST'])
+@login_required
 def edit(movie_id):
     movie = Movie.query.get_or_404(movie_id)
 
@@ -65,6 +83,7 @@ def edit(movie_id):
     return render_template('edit.html', movie=movie)
 
 @app.route('/movie/delete/<int:movie_id>', methods=['POST'])
+@login_required
 def delete(movie_id):
     movie = Movie.query.get_or_404(movie_id)
     db.session.delete(movie)
@@ -81,13 +100,11 @@ def hello():
 def initdb(drop):
     if drop:
         db.drop_all()
+        click.echo('Drop tables.')
     
-    # check if the database exists
-    if not os.path.exists(os.path.join(app.root_path, 'data.db')):
-        db.create_all()
-        click.echo('Initialized database.')
-    else:
-        click.echo('Database already exists.')
+    db.create_all()
+    click.echo('Initialized database.')
+
 
 @app.cli.command()
 def forge():
@@ -123,6 +140,77 @@ def page_not_found(e):
 def test_url_for():
     print(app.root_path)
     return 'Test page'
+
+@app.cli.command()
+@click.option('--username', prompt=True, help='The username used to login.')
+@click.option('--password', prompt=True, hide_input=True, confirmation_prompt=True, help='The password used to login.')
+def admin(username: str, password: str) -> None:
+    db.create_all()
+
+    user = User.query.first()
+    if user is not None:
+        click.echo('Updating user...')
+        user.username = username
+        user.set_password_hash(password)
+    else:
+        click.echo('Creating user...')
+        user = User(username=username, name='Admin')
+        user.set_password_hash(password)
+        db.session.add(user)
+    
+    db.session.commit()
+    click.echo('Done.')
+
+@login_manager.user_loader
+def user_loader(user_id):
+    user = User.query.get(int(user_id))
+    return user
+
+@app.route('/login', methods=['GET', 'POST'])
+def login():
+    if request.method == 'POST':
+        username = request.form['username']
+        password = request.form['password']
+    
+        if not username or not password:
+            flash('Invalid input.')
+            return redirect(url_for('login'))
+
+        user = User.query.first()
+        print(user)
+        if username == user.username and user.check_password_hash(password):
+            login_user(user)
+            flash('Login success.')
+            return redirect(url_for('index'))
+
+        flash('Invalid username or password.')
+        return redirect(url_for('login'))
+    
+    return render_template('login.html')
+
+@app.route('/logout')
+@login_required
+def logout():
+    logout_user()
+    flash('Goodbye.')
+    return redirect(url_for('index'))
+
+@app.route('/settings', methods=['GET', 'POST'])
+@login_required
+def settings():
+    if request.method == 'POST':
+        name = request.form['name']
+
+        if not name or len(name) > 20:
+            flash('Invalid input.')
+            return redirect(url_for('settings'))
+        
+        current_user.name = name
+        db.session.commit()
+        flash('Settings updated.')
+        return redirect(url_for('index'))
+    
+    return render_template('settings.html')
 
 if __name__ == "__main__":
     from waitress import serve
